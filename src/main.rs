0a28a7562680280ca49adce77f4bbcdfed5ffadc
Nyctef <nyctef@nyctef.com>
2019-12-15 18:18:00 +0000

    draw projectile path to file

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 // TODO: should this extern crate still be required?
 extern crate rtc;
+use rtc::canvas::{Canvas, TestCanvas};
+use rtc::color::Color;
 use rtc::tuple::Tuple;
+use std::fs;
 
 #[derive(Debug)]
 struct Environment {
@@ -23,17 +26,25 @@ fn tick(env: &Environment, proj: &Projectile) -> Projectile {
 fn main() {
     let mut p = Projectile {
         position: Tuple::point(0.0, 0.1, 0.0),
-        velocity: Tuple::vec(1.0, 1.0, 0.0).normalize(),
+        velocity: Tuple::vec(1.0, 1.8, 0.0).normalize() * 11.25,
     };
     let e = Environment {
         gravity: Tuple::vec(0.0, -0.1, 0.0),
         wind: Tuple::vec(-0.01, 0.0, 0.0),
     };
+    let mut c = TestCanvas::new(900, 550);
 
     while p.position.y > 0.0 {
         println!("{:?}", p);
+        c.write_pixel(
+            &Color::red(),
+            p.position.x as usize,
+            549 - (p.position.y as usize),
+        );
         p = tick(&e, &p);
     }
 
-    println!("Done")
+    println!("Done");
+
+    fs::write("output.ppm", c.to_ppm()).unwrap();
 }
