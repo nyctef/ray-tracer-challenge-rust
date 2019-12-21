9353a7ed26ab48d5073602bf93685e805245bae4
Nyctef <nyctef@nyctef.com>
2019-12-21 17:56:16 +0000

    implement transformations on Rays

--- src/rays.rs
+++ src/rays.rs
@@ -1,4 +1,6 @@
+use crate::matrixes::Matrix4;
 use crate::tuple::Tuple;
+use std::ops::Mul;
 
 #[derive(Debug)]
 pub struct Ray {
@@ -24,9 +26,18 @@ pub trait RayIntersection {
     fn ray_intersection(self, ray: Ray) -> Self::OutputType;
 }
 
+impl Mul<Ray> for Matrix4 {
+    type Output = Ray;
+
+    fn mul(self, other: Ray) -> Ray {
+        Ray::new(self * other.origin, self * other.direction)
+    }
+}
+
 #[cfg(test)]
 pub mod tests {
     use super::*;
+    use crate::transformations::{scaling, translation};
 
     #[test]
     fn creating_a_ray() {
@@ -48,5 +59,25 @@ pub mod tests {
         assert_eq!(Tuple::point(3., 3., 4.), r.position(1.));
         assert_eq!(Tuple::point(1., 3., 4.), r.position(-1.));
         assert_eq!(Tuple::point(4.5, 3., 4.), r.position(2.5));
+    }
+
+    #[test]
+    fn translating_a_ray() {
+        let r = Ray::new(Tuple::point(1., 2., 3.), Tuple::vec(0., 1., 0.));
+        let transform = translation(3., 4., 5.);
+        let r2 = transform * r;
+
+        assert_eq!(Tuple::point(4., 6., 8.), r2.origin);
+        assert_eq!(Tuple::vec(0., 1., 0.), r2.direction);
+    }
+
+    #[test]
+    fn scaling_a_ray() {
+        let r = Ray::new(Tuple::point(1., 2., 3.), Tuple::vec(0., 1., 0.));
+        let transform = scaling(2., 3., 4.);
+        let r2 = transform * r;
+
+        assert_eq!(Tuple::point(2., 6., 12.), r2.origin);
+        assert_eq!(Tuple::vec(0., 3., 0.), r2.direction);
     }
 }
