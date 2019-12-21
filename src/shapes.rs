fba331a32dcbe0038b7d932bb5ee33511d520e8e
Nyctef <nyctef@nyctef.com>
2019-12-21 21:33:50 +0000

    trying to implement normals for arbitrary spheres

--- src/shapes.rs
+++ src/shapes.rs
@@ -1,5 +1,5 @@
 use matrixes::Matrix4;
-use transformations::{scaling, translation};
+use transformations::{rotation_z, scaling, translation};
 use tuple::Tuple;
 
 #[derive(Debug, PartialEq, Copy, Clone)]
@@ -8,6 +8,10 @@ pub struct Sphere {
 }
 
 impl Sphere {
+    pub fn new(transformation: Matrix4) -> Sphere {
+        Sphere { transformation }
+    }
+
     pub fn unit() -> Sphere {
         Sphere {
             transformation: Matrix4::identity(),
@@ -24,30 +28,69 @@ impl Sphere {
 
     // TODO: possible trait?
     pub fn normal_at(&self, position: Tuple) -> Tuple {
-        Tuple::vec(position.x, position.y, position.z)
+        // TODO: should these transformations always be invertible? (see TODO in trasnformations.rs tests)
+        let world_to_sphere = self
+            .transformation
+            .try_inverse()
+            .expect("Panic! Sphere transformation not invertible!");
+
+        let object_point = world_to_sphere * position;
+        let object_normal = object_point - Tuple::point(0., 0., 0.);
+        // TODO: figure out why this works and/or is necessary
+        let mut world_normal = world_to_sphere.transpose() * object_normal;
+        world_normal.w = 0.;
+        return world_normal.normalize();
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    extern crate float_cmp;
+    use self::float_cmp::approx_eq;
+    use std::f32::consts::PI;
+
+    // TODO: learn more about how this assert is put together
+    // (code crudely copied from https://doc.rust-lang.org/src/core/macros.rs.html#78-111)
+    macro_rules! assert_tuple_eq {
+        ($left:expr, $right:expr) => {{
+            match (&$left, &$right) {
+                (left_val, right_val) => {
+                    if !approx_eq!(Tuple, *left_val, *right_val) {
+                        panic!(
+                            r#"assertion failed: `(left == right)`
+   left: `{:?}`
+  right: `{:?}`"#,
+                            &*left_val, &*right_val
+                        );
+                    }
+                }
+            }
+        }};
+    }
+
     #[test]
     fn normal_at_points_on_unit_sphere() {
         let s = Sphere::unit();
 
         // normal on x axis points in x direction
         let n1 = s.normal_at(Tuple::point(1., 0., 0.));
-        assert_eq!(Tuple::vec(1., 0., 0.), n1);
+        assert_tuple_eq!(Tuple::vec(1., 0., 0.), n1);
 
-        let root_three_third = 3_f32.sqrt() / 3_f32;
-        let n2 = s.normal_at(Tuple::point(
-            root_three_third,
-            root_three_third,
-            root_three_third,
-        ));
-        assert_eq!(
-            Tuple::vec(root_three_third, root_three_third, root_three_third),
-            n2
-        );
+        let r33 = 3_f32.sqrt() / 3_f32;
+        let n2 = s.normal_at(Tuple::point(r33, r33, r33));
+        assert_tuple_eq!(Tuple::vec(r33, r33, r33), n2);
+    }
+
+    #[test]
+    fn normal_at_points_on_translated_sphere() {
+        let s1 = Sphere::pos_r(Tuple::point(0., 1., 0.), 1.);
+        let n1 = s1.normal_at(Tuple::point(0., 1.707111, -0.70711));
+        assert_tuple_eq!(n1, Tuple::vec(0., 0.70711, -0.70711));
+
+        let s2 = Sphere::new(scaling(1., 0.5, 1.) * rotation_z(PI / 5.));
+        let s22 = 2_f32.sqrt() / 2.;
+        let n2 = s2.normal_at(Tuple::point(0., s22, -s22));
+        assert_tuple_eq!(n2, Tuple::vec(0., 0.97014, -0.24254));
     }
 }
