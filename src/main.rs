ff6d07aa1c4953cbb2830ef3f07e3bbd062ac005
Nyctef <nyctef@nyctef.com>
2019-12-23 15:26:35 +0000

    try to decouple resolution and "camera canvas size"

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,28 @@
 extern crate rtc;
-use rtc::RaySphereIntersection::*;
 use rtc::*;
 use std::fs;
 
 fn main() {
-    // TODO: decouple resolution and "camera canvas size"
-    let canvas_size = 500;
-    let cs2 = (canvas_size as f32) / 2.;
-    let mut c = TestCanvas::new(canvas_size, canvas_size);
+    let resolution = 500;
+    let mut c = TestCanvas::new(resolution, resolution);
 
     let mut material = PhongMaterial::default();
     material.color = Color::new(1., 0.2, 1.);
-    let sphere = Sphere::pos_r_m(
-        Tuple::point(0., 0., 0.),
-        4.5, /*canvas_size as f32 / 20.*/
-        material,
-    );
+    let sphere = Sphere::pos_r_m(Tuple::point(0., 0., 0.), 3., material);
 
     let light = PointLight::new(Color::new(1., 1., 1.), Tuple::point(-10., 10., -10.));
 
     let camera_origin = Tuple::point(0., 0., -5.);
+    let canvas_size = 50.;
     let canvas_z = 10_f32;
 
-    for x in 0..canvas_size {
-        for y in 0..canvas_size {
-            let x2 = (x as f32) - cs2;
-            let y2 = (y as f32) - cs2;
+    let px_per_canvas_width = resolution as f32 / canvas_size;
+
+    for x in 0..resolution {
+        for y in 0..resolution {
+            let x2 = (x as f32) / px_per_canvas_width - (canvas_size / 2.);
+            let y2 = (y as f32) / px_per_canvas_width - (canvas_size / 2.);
+            // println!("{} {}", x2, y2);
             let ray = Ray::new(
                 camera_origin,
                 (Tuple::point(x2, y2, canvas_z) - camera_origin).normalize(),
@@ -35,7 +32,7 @@ fn main() {
                 let eye = -ray.direction;
                 let color = lighting(hit.material, light, hit.point, eye, hit.surface_normal);
 
-                c.write_pixel(&color, x, canvas_size - y - 1);
+                c.write_pixel(&color, x, resolution - y - 1);
             });
         }
     }
