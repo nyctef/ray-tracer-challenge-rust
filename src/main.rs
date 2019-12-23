f0b1a9748319d26c6dbcf43e722dbb31c8649d3c
Nyctef <nyctef@nyctef.com>
2019-12-23 00:55:42 +0000

    switch from orthogonal camera to something slightly more realistic

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,3 @@
-// TODO: should this extern crate still be required?
 extern crate rtc;
 use rtc::RaySphereIntersection::*;
 use rtc::*;
@@ -6,16 +5,22 @@ use std::fs;
 
 fn main() {
     let canvas_size = 50;
+    let cs2 = (canvas_size as f32) / 2.;
     let mut c = TestCanvas::new(canvas_size, canvas_size);
 
-    let sphere = Sphere::pos_r(Tuple::point(25., 25., 0.), 20.);
+    let sphere = Sphere::pos_r(Tuple::point(0., 0., 0.), 2.);
+    let camera_origin = Tuple::point(0., 0., -5.);
+    let canvas_z = 10_f32;
 
     for x in 0..canvas_size {
         for y in 0..canvas_size {
+            let x2 = (x as f32) - cs2;
+            let y2 = (y as f32) - cs2;
             let ray = Ray::new(
-                Tuple::point(x as f32, y as f32, -50.),
-                Tuple::vec(0., 0., 1.),
+                camera_origin,
+                (Tuple::point(x2, y2, canvas_z) - camera_origin).normalize(),
             );
+            println!("{:?}", ray);
             let intersects = sphere.ray_intersection(ray);
             match intersects {
                 Misses => c.write_pixel(&Color::black(), x, y),
